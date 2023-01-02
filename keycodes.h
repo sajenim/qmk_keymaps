67e7d2c0797483918a6541a7a913ced6cda98db4
sajenim <its.jassy@pm.me>
2023-01-02 09:11:33 +0800

    refactor

--- keycodes.h
+++ keycodes.h
@@ -1,25 +1,7 @@
 // This file contains user defined aliases
 
-// Default layouts
-#define QWERTY  DF(_QWERTY)
-#define COLEMAK DF(_COLEMAK)
-#define DVORAK  DF(_DVORAK)
-#define CANARY  DF(_CANARY)
-
-// Extra layers
+// Layers
 #define SPACEFN LT(_SPACEFN, KC_SPC)
-
-// Left-hand home row mods
-#define HOME_A  LGUI_T(KC_A)
-#define HOME_R  LALT_T(KC_R)
-#define HOME_S  LSFT_T(KC_S)
-#define HOME_T  LCTL_T(KC_T)
-
-// Right-hand home row mods
-#define HOME_N  RCTL_T(KC_N)
-#define HOME_E  RSFT_T(KC_E)
-#define HOME_I  RALT_T(KC_I)
-#define HOME_O  RGUI_T(KC_O)
 
 // Dead keys
 #define DK_MOD4 OSM(MOD_LGUI)
@@ -28,3 +10,15 @@
 
 // Shortcuts
 #define DELWORD LCTL(KC_W)
+
+// Left-hand home row mods
+#define HOME_C  LGUI_T(KC_C)
+#define HOME_R  LALT_T(KC_R)
+#define HOME_S  LSFT_T(KC_S)
+#define HOME_T  LCTL_T(KC_T)
+
+// Right-hand home row mods
+#define HOME_N  RCTL_T(KC_N)
+#define HOME_E  RSFT_T(KC_E)
+#define HOME_I  RALT_T(KC_I)
+#define HOME_A  RGUI_T(KC_A)
