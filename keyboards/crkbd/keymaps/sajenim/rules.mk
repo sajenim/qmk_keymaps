1f19f8b209241f92b24ad87a90888089e236feff
sajenim <its.jassy@pm.me>
2024-02-05 11:06:05 +0800

    refactor

--- keyboards/crkbd/keymaps/sajenim/rules.mk
+++ keyboards/crkbd/keymaps/sajenim/rules.mk
@@ -1,30 +1,36 @@
-# We are using a Splinky RP2040 as a drop-in replacement controller
+# We are using a Splinky RP2040 as a drop-in pro-micro replacement
 CONVERT_TO = promicro_rp2040
 BOOTLOADER = rp2040
 
-# Default features 
+# Reduce compiled size of firmware
 LTO_ENABLE = yes
 
-# Optional features
-OLED_ENABLE 				= yes
-RGBLIGHT_ENABLE			= yes
-REPEAT_KEY_ENABLE		= yes
-LEADER_ENABLE				= yes
-# Toggleable through keymap
-AUTO_SHIFT_ENABLE		= yes
-AUTOCORRECT_ENABLE	= yes
-CAPS_WORD_ENABLE  	= yes
-COMBO_ENABLE 				= yes
+# Software features
+AUTOCORRECT_ENABLE		= yes
+AUTO_SHIFT_ENABLE			= yes
+CAPS_WORD_ENABLE			= yes
+COMBO_ENABLE					= yes
+DYNAMIC_MACRO_ENABLE	= yes
+LEADER_ENABLE					= yes
+MOUSEKEY_ENABLE				= yes
+OLED_ENABLE						= yes
+REPEAT_KEY_ENABLE			= yes
+RGBLIGHT_ENABLE				= yes
 
-SRC += process_record.c
+# Feature libraries
 SRC += features/sentence_case.c
 
+# Behaviour of our custom keycodes
+SRC += process_record.c
+
+# Oled display configuration
 ifeq ($(OLED_ENABLE),yes)
 	SRC += ./oled.c
 	OLED_DRIVER = ssd1306
 	WPM_ENABLE = yes
 endif
 
+# Combo dictionary
 ifeq ($(COMBO_ENABLE),yes)
 	VPATH += keyboards/gboards
 endif
