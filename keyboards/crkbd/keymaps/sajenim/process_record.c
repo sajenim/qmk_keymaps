e822704000d8b5ab184f8d60219637f47ad81bb5
sajenim <its.jassy@pm.me>
2024-02-05 11:02:51 +0800

    remove bspc_dw

--- keyboards/crkbd/keymaps/sajenim/process_record.c
+++ keyboards/crkbd/keymaps/sajenim/process_record.c
@@ -4,15 +4,6 @@
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_sentence_case(keycode, record)) { return false; }
-    
-    switch (keycode) {
-        case BSPC_DW:
-            if (record->tap.count && record->event.pressed) {
-                tap_code(KC_BSPC);
-            } else if (record->event.pressed) {
-                tap_code16(C(KC_W));
-            }
-            return false;
 
         case SC_TOGG:
             if (record->event.pressed) {
