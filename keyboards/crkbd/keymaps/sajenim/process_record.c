868427d38801216ee0c7196f2ca22232fe41645c
sajenim <its.jassy@pm.me>
2024-02-05 11:03:11 +0800

    fix formatting

--- keyboards/crkbd/keymaps/sajenim/process_record.c
+++ keyboards/crkbd/keymaps/sajenim/process_record.c
@@ -2,14 +2,17 @@
 #include "layers.h"
 #include "features/sentence_case.h"
 
+// Define the behaviour of our custom keycodes
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!process_sentence_case(keycode, record)) { return false; }
+  if (!process_sentence_case(keycode, record)) { return false; }
 
-        case SC_TOGG:
-            if (record->event.pressed) {
-                sentence_case_toggle();
-            }
-            return false;
-  }
+  switch (keycode) {
+    // Toggle sentence case.
+    case SC_TOGG:
+      if (record->event.pressed) {
+        sentence_case_toggle();
+      }
+      return false; // Skip all further processing of this key
+  } 
   return true;
 };
