019a178ad43cefa1517e4e73bf58d64630399542
jasmine <its.jassy@pm.me>
2024-12-03 21:31:56 +0800

    update keymap

--- keyboards/crkbd/keymaps/sajenim/keymap.c
+++ keyboards/crkbd/keymaps/sajenim/keymap.c
@@ -104,13 +104,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [NUM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, KC_MINS,    KC_7,    KC_8,    KC_9, KC_SLSH,                      XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
+      XXXXXXX, KC_SLSH,    KC_7,    KC_8,    KC_9, KC_MINS,                      XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_PLUS,    KC_4,    KC_5,    KC_6, KC_ASTR,                      XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11, _______,
+      XXXXXXX, KC_ASTR,    KC_4,    KC_5,    KC_6, KC_PLUS,                      XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,    KC_0,    KC_1,    KC_2,    KC_3,  KC_DOT,                      XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F12, XXXXXXX,
+      XXXXXXX,  KC_DOT,    KC_1,    KC_2,    KC_3,  KC_EQL,                      XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F12, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, _______, _______,    XXXXXXX, _______, XXXXXXX
+                                          XXXXXXX,    KC_0, _______,    XXXXXXX, _______, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
